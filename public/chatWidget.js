0b74b15c38ea2056595ece879d64ce82bbd26469
RajKKapadia <raajforyou@gmail.com>
2024-12-15 11:21:41 +0530

    fixed port number in the chat widget

--- public/chatWidget.js
+++ public/chatWidget.js
@@ -160,7 +160,7 @@
     // Function to call OpenAI API with conversation history
     const fetchOpenAIResponse = async () => {
         try {
-            const response = await fetch('http://127.0.0.1:5000/api/chat', {
+            const response = await fetch('http://127.0.0.1:3000/api/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
